852ca68f7739ce6145755d31903c3dacbef999f8
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2021-08-01 23:45:33 -0500

    Finished Proyect

---
+++ Programming-Theory-Repo/ProgrammingTheoryRepo/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text nameTxt;
+    public Text songTxt;
+
+    public void SettingDua()
+    {
+        DuaLipa duaLipa = new DuaLipa();
+        nameTxt.text = duaLipa.name;
+        songTxt.text = duaLipa.song;
+    }
+
+    public void SettingFredy()
+    {
+        Fredy fredy = new Fredy();
+        nameTxt.text = fredy.name;
+        songTxt.text = fredy.song;
+    }
+    public void SettingBadBunny()
+    {
+        BadBunny badBunny = new BadBunny();
+        nameTxt.text = badBunny.name;
+        songTxt.text = badBunny.song;
+    }
+}
